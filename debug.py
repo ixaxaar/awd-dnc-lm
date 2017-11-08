2e3f66c40db8de554f389a19c0c325bc1f00581a
ixaxaar <root@ixaxaar.in>
2017-11-08 12:17:20 +0530

    Script to debug memory in vidsom

---
+++ debug.py
@@ -0,0 +1,204 @@
+###############################################################################
+# Language Modeling on Penn Tree Bank
+#
+# This file is used to visualize memory contents in visdom
+#
+###############################################################################
+
+import argparse
+
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+import data
+from visdom import Visdom
+
+viz = Visdom()
+
+parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
+
+# Model parameters.
+parser.add_argument('--data', type=str, default='./data/penn',
+                    help='location of the data corpus')
+parser.add_argument('--checkpoint', type=str, default='./model.pt',
+                    help='model checkpoint to use')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed')
+parser.add_argument('--nbest', type=int, default=5,
+                    help='top n results')
+parser.add_argument('--cuda', type=int, default=-1, help='Cuda GPU ID, -1 for CPU')
+parser.add_argument('--temperature', type=float, default=1.0,
+                    help='temperature - higher will increase diversity')
+parser.add_argument('--log-interval', type=int, default=100,
+                    help='reporting interval')
+parser.add_argument('--debug', action='store_true',
+                    help='debug DNC memory contents in visdom (on localhost)')
+parser.add_argument('--input', type=str, default='shares of ual the parent of united airlines were extremely active all day friday reacting to news and rumors about the proposed $ N billion buy-out of the airline by an <unk> group',
+                    help='Input sentence to debug')
+
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if args.cuda == -1:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda <GPU_ID>")
+    else:
+        torch.cuda.manual_seed(args.seed)
+
+if args.temperature < 1e-3:
+    parser.error("--temperature has to be greater or equal 1e-3")
+
+with open(args.checkpoint, 'rb') as f:
+    model = torch.load(f)
+model.eval()
+
+if args.cuda != -1:
+    model.cuda(args.cuda)
+else:
+    model.cpu()
+
+corpus = data.Corpus(args.data)
+ntokens = len(corpus.dictionary)
+hidden = model.init_hidden(1)
+
+raw_input = args.input.split()
+input = Variable(torch.Tensor([ corpus.dictionary.word2idx[x] for x in raw_input ]).long(), volatile=True).unsqueeze(1)
+raw_input.append('<eos>')
+
+# input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
+if args.cuda != -1:
+    input = input.cuda(args.cuda)
+
+mem_debug = {
+    "memory": [],
+    "link_matrix": [],
+    "precedence": [],
+    "read_weights": [],
+    "write_weights": [],
+    "usage_vector": [],
+}
+
+debug = []
+out_words = []
+
+if model.debug:
+    output, hidden, v = model(input, hidden, reset_experience=True)
+    debug.append(v)
+else:
+    output, hidden = model(input, hidden, reset_experience=True)
+
+word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
+ppx = [ torch.topk(w, args.nbest)[0] for w in word_weights ]
+# print(ppx)
+ppx = [ p[0] for p in ppx ]
+ppx = np.array(ppx)
+ppxt = np.arange(ppx.shape[0])
+# ppx = np.array([ppx, ppxt])
+print(ppx)
+word_idx = [ torch.topk(w, args.nbest)[1] for w in word_weights ]
+words = [ [ corpus.dictionary.idx2word[w] for w in ws ] for ws in word_idx ]
+
+mem_debug = v[0]
+total_len = len(words)
+matched = 0
+for i,w in enumerate(words):
+    print(w)
+    out_words.append(str(i)+"......................"+raw_input[i+1])
+    if raw_input[i+1] in w:
+        out_words.append(str(i+1)+"********************************")
+        matched += 1
+    else:
+        out_words.append(str(i+1)+".   ("+w[0]+")")
+
+print("Matched: %s / %s" % (matched, total_len-1))
+
+if args.debug:
+
+    print("=====================================================")
+    print(model)
+    print("=====================================================")
+
+    for k,v in mem_debug.items():
+        print(k, v.shape)
+
+    viz.line(
+        X=ppx,
+        Y=ppxt,
+        opts=dict(
+            colormap='Viridis',
+            title='DNC output',
+            ylabel='time',
+            xlabel='value',
+            markers=True,
+            fillarea=False
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['memory'],
+        opts=dict(
+            colormap='Viridis',
+            title='Memory',
+            ylabel='layer * time',
+            xlabel='mem_slot * mem_size',
+            rownames=out_words
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['link_matrix'],
+        opts=dict(
+            colormap='Viridis',
+            rownames=out_words,
+            title='Link Matrix',
+            ylabel='layer * time',
+            xlabel='mem_slot * mem_slot'
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['precedence'],
+        opts=dict(
+            colormap='Viridis',
+            rownames=out_words,
+            title='Precedence',
+            ylabel='layer * time',
+            xlabel='mem_slot'
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['read_weights'],
+        opts=dict(
+            colormap='Viridis',
+            rownames=out_words,
+            title='Read Weights',
+            ylabel='layer * time',
+            xlabel='nr_read_heads * mem_slot'
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['write_weights'],
+        opts=dict(
+            rownames=out_words,
+            colormap='Viridis',
+            title='Write Weights',
+            ylabel='layer * time',
+            xlabel='mem_slot'
+        )
+    )
+
+    viz.heatmap(
+        mem_debug['usage_vector'],
+        opts=dict(
+            colormap='Viridis',
+            rownames=out_words,
+            title='Usage Vector',
+            ylabel='layer * time',
+            xlabel='mem_slot'
+        )
+    )
+
